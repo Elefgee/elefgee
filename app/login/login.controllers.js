67cfc32ee850b8521f0269f0b26a9b750544fbfb
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-07-23 09:42:31 -0400

    Began working on views.

--- app/login/login.controllers.js
+++ app/login/login.controllers.js
@@ -5,11 +5,11 @@
     .controller('LoginController', function($scope, $route, SteamService) {
       $scope.$route = $route;
 
-      SteamService.buildPlayerUrl('ISteamUser', 'GetPlayerSummaries', '0002', '76561197960435530');
+      // SteamService.buildPlayerUrl('ISteamUser', 'GetPlayerSummaries', '0002', '76561197960435530');
 
       SteamService.getPlayerSummary('ISteamUser', 'GetPlayerSummaries', '0002', '76561197960435530');
 
-      $scope.urlTest = SteamService.buildPlayerUrl('one', 'two', 'three');
+      // $scope.urlTest = SteamService.buildPlayerUrl('one', 'two', 'three');
 
     })
 })();
--- app/steam/steam.services.js
+++ app/steam/steam.services.js
@@ -4,17 +4,25 @@
     .module('elefgee')
     .factory('SteamService', function ($http, _, $q, $cacheFactory) {
       var playerUrlOpts = {
-        baseUrl: 'http://api.steampowered.com',
+        baseUrl: 'https://api.steampowered.com',
         apiKey: '41AB27857C781D410407E14B482DB2ED',
         buildUrl: function (interfaceName, methodName, versionName, steamId) {
-          var url = playerUrlOpts.baseUrl + '/' + interfaceName + '/' + methodName + '/v' + versionName + '/?key=' + playerUrlOpts.apiKey + '&steamids=' + steamId;
+          var url = playerUrlOpts.baseUrl + '/' + interfaceName + '/' + methodName + '/v' + versionName + "/?callback=JSON_CALLBACK" + '&key=' + playerUrlOpts.apiKey + '&steamids=' + steamId + "&full=1";
           console.log(url);
           return url;
         }
       };
 
       var getPlayerSummary = function(interfaceName, methodName, versionName, steamId) {
-        $http.get(playerUrlOpts.buildUrl(interfaceName, methodName, versionName, steamId)).then(function (summary) {
+        var request = {
+          method: 'JSONP',
+          url: playerUrlOpts.buildUrl(interfaceName, methodName, versionName, steamId),
+          headers: {
+            "Content-Type": "*/*",
+            "Origins": "*"
+          }
+        }
+        $http(request).success(function() {
           console.log('SUMMARY: ', summary);
           var playerSummary = summary.players;
           return playerSummary;
