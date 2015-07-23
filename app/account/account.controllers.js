405219273de51cef74ffc533b08104e61feb390d
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-23 12:16:41 -0400

    accountPage toggle nav

--- app/account/account.controllers.js
+++ app/account/account.controllers.js
@@ -2,7 +2,35 @@
   'use strict';
   angular
     .module('account')
-    .controller('AccountController', function($scope, $route, SteamService) {
+    .controller('AccountController', function($scope, $route, SteamService, _) {
+      $scope.navs = [
+        {
+          name: 'account',
+          active: true
+        },
+        {
+          name: 'library',
+          active: false
+        },
+        {
+          name: 'posts',
+          active: false
+        }
+      ]
 
+      $scope.accountNavClick = function(clicked) {
+        _.each($scope.navs, function(el){
+          el.active = false;
+        });
+       var clickedBtn = _.findWhere($scope.navs, {name: clicked});
+       clickedBtn.active = true;
+
+       return clickedBtn.active;
+      }
+
+      $scope.isTrue = function(clicked) {
+        var nameBtn = _.findWhere($scope.navs, {name: clicked})
+        return nameBtn.active;
+      }
     })
 })();
