8c927bdebf29c801cc618e538f689ab4e8e83017
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-26 17:15:24 -0400

    getting data back from database

---
+++ steam-boilerplate-master/app/feed/feed.controllers.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+  angular
+    .module('elefgee')
+    .controller('FeedController', function($scope, $route, SteamService) {
+
+      console.log($scope.user);
+
+      SteamService.getUserInfo().success(function(data){
+        console.log(data);
+      });
+
+
+      $scope.$route = $route;
+      $scope.number = 5;
+      $scope.getNumber = function(num) {
+        return new Array(num);
+      }
+    })
+})();
---
+++ steam-boilerplate-master/app/steam/steam.services.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+  angular
+    .module('elefgee')
+    .factory('SteamService', function ($http, _, $q, $cacheFactory) {
+      var playerUrlOpts = {
+        baseUrl: 'https://api.steampowered.com',
+        apiKey: '41AB27857C781D410407E14B482DB2ED',
+        buildUrl: function (interfaceName, methodName, versionName, steamId) {
+          var url = playerUrlOpts.baseUrl + '/' + interfaceName + '/' + methodName + '/v' + versionName + "/?callback=JSON_CALLBACK" + '&key=' + playerUrlOpts.apiKey + '&steamids=' + steamId + "&full=1";
+          console.log(url);
+          return url;
+        }
+      };
+
+      var getUserInfo = function() {
+        return $http.get('/user');
+      }
+
+      return {
+        buildPlayerUrl: playerUrlOpts.buildUrl,
+        getUserInfo: getUserInfo
+      };
+
+    });
+})();
