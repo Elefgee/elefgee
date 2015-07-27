774299004c1fd4e4d63c367affb69a5b6185477f
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-27 15:37:29 -0400

    heroku

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new SteamStrategy({
-    returnURL: 'https://elefgeesam.com/auth/steam/return',
-    realm: 'https://elefgeesam.com/',
+    returnURL: 'https://localhost:3000/auth/steam/return',
+    realm: 'https://localhost:3000/',
     apiKey: '41AB27857C781D410407E14B482DB2ED'
   },
   function(identifier, profile, done) {
