7fbd00a04827adbf4a46f875139f72d195e665f5
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-29 12:21:40 -0400

    a

--- app/post/post.controllers.js
+++ app/post/post.controllers.js
@@ -2,7 +2,7 @@
   'use strict';
   angular
     .module('elefgee')
-    .controller('PostController', function($scope, $route, SteamService) {
+    .controller('PostController', function($scope, $route, SteamService, $rootScope) {
       $scope.$route = $route;
       $scope.number = 15;
       $scope.getNumber = function(num) {
@@ -17,8 +17,13 @@
       $scope.selectGame = function($event) {
         console.log($event.currentTarget);
         var target = $event.currentTarget;
+        var targetName = $(target).data('id');
+        var targetId = $(target).children().data('id');
+        $rootScope.selectedGameName = targetName;
+        $rootScope.selectedGameId = targetId;
         $(target).siblings().removeClass('selectedGame');
         $(target).addClass('selectedGame');
       }
     })
+
 })();
