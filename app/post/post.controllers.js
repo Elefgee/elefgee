2b1407d839a0cef1175518ef5e148a2afc1966de
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-29 13:47:36 -0400

    heroku

--- app/post/post.controllers.js
+++ app/post/post.controllers.js
@@ -5,19 +5,28 @@
     .controller('PostController', function($scope, $rootScope, $route, SteamService, _) {
       $scope.$route = $route;
 
+      $scope.post = {};
+
       SteamService.getMe().success(function(data){
         $scope.me = data;
         $scope.games = data.games
+        $scope.post.userData = data;
       })
 
       $scope.selectGame = function($event) {
         console.log($event.currentTarget);
         var target = $event.currentTarget;
         var targetId = $(target).data('id');
+        var selectedGames = _.where($scope.games.games, {appid: targetId});
         $rootScope.selectedGame = _.where($scope.games.games, {appid: targetId});
-        console.log('SELECTEDGAME', $rootScope.selectedGame[0]);
+        $scope.post.name = selectedGames[0].name;
+        $scope.post.appid = selectedGames[0].appid;
         $(target).siblings().removeClass('selectedGame');
         $(target).addClass('selectedGame');
+      }
+
+      $scope.addPost = function(postData) {
+        SteamService.addPost(postData)
       }
     })
 
