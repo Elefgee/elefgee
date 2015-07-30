8e48fe338e339c63af01f24bc4a669f45a45a0d9
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-07-30 16:39:43 -0400

    Made form verifiable

--- app/steam/steam.services.js
+++ app/steam/steam.services.js
@@ -3,15 +3,6 @@
   angular
     .module('elefgee')
     .factory('SteamService', function ($http, _, $q, $cacheFactory) {
-      var playerUrlOpts = {
-        baseUrl: 'https://api.steampowered.com',
-        apiKey: '41AB27857C781D410407E14B482DB2ED',
-        buildUrl: function (interfaceName, methodName, versionName, steamId) {
-          var url = playerUrlOpts.baseUrl + '/' + interfaceName + '/' + methodName + '/v' + versionName + "/?callback=JSON_CALLBACK" + '&key=' + playerUrlOpts.apiKey + '&steamids=' + steamId + "&full=1";
-          console.log(url);
-          return url;
-        }
-      };
       var getUserInfo = function() {
         return $http.get('/user');
       }
@@ -23,15 +14,7 @@
          console.log('I just posted this data!', data);
         })
       }
-
-      var addPost = function(post) {
-        $http.put('/posts', post).success(function(data){
-          console.log('I just posted this data!', data);
-        })
-      }
-
       return {
-        buildPlayerUrl: playerUrlOpts.buildUrl,
         getUserInfo: getUserInfo,
         addPost: addPost,
         getMe: getMe
